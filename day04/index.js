3c8214a97e2d280be124e6aef1c86153a57498b6
Zachary Pitcher <zackpi@mit.edu>
2025-12-04 08:31:15 -0500

    neighbor update

--- day04/index.js
+++ day04/index.js
@@ -2,51 +2,56 @@ let raw = await Bun.file("day04/input").text();
 // let raw = await Bun.file("day04/input_small").text();
 let grid = raw.split("\n").map(l => l.split(""));
 
-let remove = (grid) => {
-  let countGrid = grid.map(row => row.map(_ => Infinity));
+let deltas = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1],           [0, 1],
+  [1, -1],  [1, 0],  [1, 1],
+];
 
-  for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < grid[r].length; c++) {
-      if (grid[r][c] === "@") {
-        let deltas = [
-          [-1, -1], [-1, 0], [-1, 1],
-          [0, -1],          [0, 1],
-          [1, -1], [1, 0], [1, 1],
-        ];
-        let count = 0;
-        for (let [dr, dc] of deltas) {
-          let nr = r + dr;
-          let nc = c + dc;
-          if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[r].length) {
-            if (grid[nr][nc] === "@") {
-              count++;
+let countGrid = grid.map(row => row.map(_ => Infinity));
+let freeList = [];
+for (let r = 0; r < grid.length; r++) {
+  for (let c = 0; c < grid[r].length; c++) {
+    if (grid[r][c] === "@") {
+
+      let count = 0;
+      for (let [dr, dc] of deltas) {
+        if (r + dr >= 0 && r + dr < grid.length && c + dc >= 0 && c + dc < grid[r].length) {
+          if (grid[r + dr][c + dc] === "@") count++;
+        }
+      }
+      countGrid[r][c] = count;
+      if (count < 4) freeList.push([r, c]); 
+      
+    }
+  }
+}
+
+let total = 0;
+let rounds = [freeList.length];
+while (freeList.length > 0) {
+  let newFreeList = [];
+  for (let [r, c] of freeList) {
+    if (grid[r][c] === "@") {
+      grid[r][c] = ".";
+      total++;
+
+      for (let [dr, dc] of deltas) {
+        if (r + dr >= 0 && r + dr < grid.length && c + dc >= 0 && c + dc < grid[r].length) {
+          if (grid[r + dr][c + dc] === "@") {
+            countGrid[r + dr][c + dc]--;
+            if (countGrid[r + dr][c + dc] === 3) {
+              newFreeList.push([r + dr, c + dc]);
             }
           }
         }
-        countGrid[r][c] = count;
       }
     }
   }
-
-  let total = 0;
-  for (let r = 0; r < countGrid.length; r++) {
-    for (let c = 0; c < countGrid[r].length; c++) {
-      if (countGrid[r][c] < 4) {
-        grid[r][c] = ".";
-        total++;
-      }
-    }
-  }
-
-  return total;
+  rounds.push(newFreeList.length);
+  freeList = newFreeList;
 }
 
-let numRemoved = remove(grid);
-let rounds = [numRemoved];
-while (numRemoved > 0) {
-  numRemoved = remove(grid);
-  rounds.push(numRemoved);
-}
 
 let part1 = rounds[0];
 console.log("part1 =", part1);
